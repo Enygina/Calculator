451432eb4bac223f4d25a271fb4fa139fe49b4a6
Olga Enygina <olga.enygina@bk.ru>
2022-12-18 17:06:12 +0300

    Initial commit

---
+++ guid.py
@@ -0,0 +1,8 @@
+import arithmetic.div as div
+import arithmetic.mult as mul
+import arithmetic.sub as sub
+import arithmetic.sum as sum
+
+
+dict_ar = {'/': div, '*': mul, '-': sub, '+': sum}
+dict_log = {'/': 'деление', '*': 'умножение', '-': 'вычитание', '+': 'сложение'}
---
+++ logger.py
@@ -0,0 +1,10 @@
+
+from datetime import datetime as dt
+from guid import dict_log
+
+
+def get_log(res, oper):
+    dtime = (dt.now()).replace(microsecond=0)
+    with open('log.txt', 'a', encoding='utf-8') as file:
+        file.write('{}; операция: {}; результат: {}\n'
+                     .format(dtime, oper, res))
